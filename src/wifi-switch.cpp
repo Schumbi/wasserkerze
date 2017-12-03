a8c29e0109fd0300e91847b61b4b2a1d0d50c32b
Ralf Warmuth <r.warmuth@schumbi.de>
2017-12-03 02:24:20 +0100

    try to fix not working leds...

--- src/wifi-switch.cpp
+++ src/wifi-switch.cpp
@@ -29,27 +29,14 @@
 
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
-#include <TickerScheduler.h>''
 
 #include "strip.h"
 #include "../../wlan.conf"
 
 // Update these with values suitable for your network.
 
-void setup_wifi();
-void callback(char* topic, byte* payload, unsigned int length);
-
-const char* ssid = MAKELIGHT_SSID;
-const char* password = MAKELIGHT_PASS;
-const char* mqtt_server = "192.168.3.1";
-
-WiFiClient espClient;
-PubSubClient client(espClient);
-long lastMsg = 0;
-char msg[50];
 int value = 0;
 
-#define LED_TICK 4
 #define MOTOR D0
 #define LED1 D5
 #define LED2 D6
@@ -58,9 +45,6 @@ int value = 0;
 
 void update_leds();
 void blink();
-void update_mqtt_status();
-
-TickerScheduler ticker(5);
 
 void setup() {
   pinMode(D0, OUTPUT);
@@ -69,95 +53,23 @@ void setup() {
   pinMode(D8, OUTPUT);
   pinMode(BUILTIN_LED, OUTPUT);     // Initialize the BUILTIN_LED pin as an output
   Serial.begin(115200);
-  setup_wifi();
-  client.setServer(mqtt_server, 1883);
-  client.setCallback(callback);
-
-  ticker.add(0, LED_TICK, update_leds);
-  ticker.add(1, 1000, update_mqtt_status);
-  ticker.add(2, 1000, blink);
-
 
   // LED Strip acitvate
   Serial.println("Strip");
   CLedStrip* strip = CLedStrip::getStrip_ptr();
   strip->init();
-  strip->getConf().ctr = 5;
+  strip->getConf().ctr = 0;
+  strip->getConf().max = 100;
   strip->getConf().min = 0;
   strip->getConf().period = 5;
   strip->switch_program(1);
   Serial.println("Strip end");
-
 }
 
-void setup_wifi() {
-
-  delay(10);
-  // We start by connecting to a WiFi network
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
-
-  WiFi.begin(ssid, password);
-
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
-}
-
-void callback(char* topic, byte* payload, size_t length) {
-  Serial.print("Message arrived [");
-  Serial.print(topic);
-  Serial.print("] ");
-  for (size_t i = 0; i < length; i++) {
-    Serial.print((char)payload[i]);
-  }
-  Serial.println();
-
-  // Switch on the LED if an 1 was received as first character
-  if ((char)payload[0] == '1') {
-    digitalWrite(BUILTIN_LED, LOW);   // Turn the LED on (Note that LOW is the voltage level
-    // but actually the LED is on; this is because
-    // it is acive low on the ESP-01)
-  } else {
-    digitalWrite(BUILTIN_LED, HIGH);  // Turn the LED off by making the voltage HIGH
-  }
-
-}
-
-void reconnect() {
-  // Loop until we're reconnected
-  while (!client.connected()) {
-    Serial.print("Attempting MQTT connection...");
-    // Attempt to connect
-    if (client.connect("ESP8266Client")) {
-      Serial.println("connected");
-      // Once connected, publish an announcement...
-      client.publish("/home/test", "hello world");
-      // ... and resubscribe
-      client.subscribe("/home/makelight");
-    } else {
-      Serial.print("failed, rc=");
-      Serial.print(client.state());
-      Serial.println(" try again in 5 seconds");
-      // Wait 5 seconds before retrying
-      delay(5000);
-    }
-  }
-}
-void loop() {
-
-  ticker.update();
-  if (!client.connected()) {
-    reconnect();
-  }
-  client.loop();
+void loop()
+{
+  update_leds();
+  //delay(100);
 }
 
 void update_leds()
@@ -186,17 +98,4 @@ void blink()
     digitalWrite(LED3, LOW);
     onOff = true;
   }
-}
-
-void update_mqtt_status()
-{
-    long now = millis();
-    if (now - lastMsg > 2000) {
-      lastMsg = now;
-      ++value;
-      snprintf (msg, 75, "hello world #%d", value);
-      Serial.print("Publish message: ");
-      Serial.println(msg);
-      client.publish("/home/test", msg);
-    }
 }
